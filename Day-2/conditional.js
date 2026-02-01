c0b526e1fda6ab1f298f45819beec81fafd4f246
Rajat Saini <webdevrar@gmail.com>
2026-02-01 23:42:27 +0530

    JS Day-2 -> some Conditional program practice, and also learn what is readline and how its work to print number in console.

---
+++ Day-2/conditional.js
@@ -0,0 +1,78 @@
+// ? BEGINNER (if / else)
+
+//* Write a program to check whether a number is positive, negative, or zero.
+ const rl = require("../Utils/rl")
+
+// rl.question("Enter number: ", function(n){
+//     let num = Number(n)
+//     if(num === 0) return done("Number is zero");
+//     if(num > 0) return done("Number is positive");
+//     return done("Number is negative");
+
+
+//     function done(msg){
+//         console.log(msg);
+//         rl.close();
+//     }
+// })
+
+
+
+//* Write a program to check whether a number is even or odd.
+
+// rl.question("Enter number: ", function(n){
+//     let num = Number(n)
+    
+//     if(num % 2 === 0)return check("Even");
+//     return check("Odd");
+
+//     function check(msg){
+//         console.log(msg);
+//         rl.close();
+//     }
+// })
+
+//* Write a program to check whether a person is eligible to vote (age ≥ 18).
+
+// rl.question("Enter age: ", function(n){
+//     let age = Number(n)
+//     if(age >= 18)return check("Person is eligible to vote");
+//     return check("Person is not eligible to vote");
+
+//     function check(msg){
+//         console.log(msg);
+//         rl.close();
+//     }
+// })
+//* Write a program to find the greater of two numbers.
+
+// rl.question("Enter first number: ", function(fn){
+//     rl.question("Enter second number: ", function(sn){
+//         let first = Number(fn)
+//         let second = Number(sn)
+
+//         if(first > second) return check(first+" : is greater")
+//         if(second > first) return check(second+": is grater") 
+//         return check("Both are equal")
+//     })
+
+//     function check(msg){
+//         console.log(msg)
+//         rl.close();
+//     }
+// })
+
+
+//* Write a program to check whether a number is divisible by 5.
+
+// rl.question("Enter number: ", function(n){
+//     let num = Number(n)
+
+//     if(num % 5 === 0) return check(num+" : number divisible by 5")
+//     return check(num+" : number is note divisible by 5")
+
+//     function check(msg){
+//         console.log(msg)
+//         rl.close();
+//     }
+// })
