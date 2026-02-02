2e02a0f7a0e5a96d191f77d2abc3a79bcb10a49d
Rajat Saini <webdevrar@gmail.com>
2026-02-02 19:44:03 +0530

    JS Day-2 -> more practice

--- Day-2/conditional.js
+++ Day-2/conditional.js
@@ -1,7 +1,7 @@
 // ? BEGINNER (if / else)
 
 //* Write a program to check whether a number is positive, negative, or zero.
- const rl = require("../Utils/rl")
+//  const rl = require("../Utils/rl")
 
 // rl.question("Enter number: ", function(n){
 //     let num = Number(n)
@@ -76,3 +76,62 @@
 //         rl.close();
 //     }
 // })
+
+//* Ask user for password and print access status, Hardcoded correct password. Compare with user input.
+
+// rl.question("Enter password : ", function(paw) {
+//     let userPaw = Number(paw);
+//     let password = 2345;
+//     if(userPaw === password) return check("Access")
+//     return check("Access Denied")
+
+//     function check(msg){
+//         console.log(msg)
+//         rl.close();
+//     }
+// })
+
+//* Allow only 3 attempts to enter correct password, If user gets it right early, stop. If not → “Account locked”
+ 
+// let setPwd = 3478;
+// let attempts = 1;
+
+// while (true) {
+
+//     let input = prompt("Enter password : ");
+//     if (input.trim() === "") {
+//         alert("Input cannot be empty");
+//     } 
+//     else if (!/^\d+$/.test(input)) {
+//         alert("Please enter only number");
+//     } 
+//     else {
+//         let userPwd = Number(input);
+
+//         if (userPwd === setPwd) {
+//             alert("Welcome Back");
+//             break;
+//         }
+//         if (attempts === 3) {
+//             alert("Account Locked");
+//             break;
+//         }
+//         attempts++;
+//     }
+// }
+
+
+//* Ask user for words until they type “stop”. Count how many times they typed “yes”, Loop until "stop" is typed. Count "yes".
+
+// let count = 0;
+// while(true){
+    
+//     let input = prompt("Enter word: ").trim().toLowerCase();
+//     if(input === "yes"){
+//         count++
+//     }
+//     else if(input === "stop"){
+//         alert(`You type (Yes): ${count} times`)
+//         break;
+//     }
+// }
