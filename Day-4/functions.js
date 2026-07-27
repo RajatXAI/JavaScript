ee1ccc8afb5d18d050fe8eb40d8aba94247c5fdc
RajatXAI <webdevrar@gmail.com>
2026-07-28 02:19:35 +0530

    Day-4 functions

--- Day-4/functions.js
+++ Day-4/functions.js
@@ -1,145 +1,305 @@
-//? Callback Function
+//? Callback + Closures Function Practice
 
-//* Q1. Write a function execute that takes a function as a parameter and calls it.
+//* LEVEL 1 - Controlled Counter System
 
-// function execute(cb){
-//     cb();
+//* Q1. Create a Secure Counter System
+
+//* Goal:
+//* Create a Function createCounter
+
+//* Requirements:
+//* It should NOT expose the count variable directly
+//* It should return an object with:
+//* increment(callback)
+//* decrement(callback)
+//* getValue()
+
+//* BEHAVIOR:
+//* count must be private(closure)
+//* Every time increment/descrement runs:
+//* Update count
+//* Execute callback and pass updates count
+
+//     function backAccount(){
+//         let balance = 1000
+
+//         return {
+
+//             deposite(amount){
+//                 balance += amount
+//                 console.log(`You are deposite ${amount} rupee and you updated balance is : ${balance}`)
+//             },
+
+//             withdraw(amount){
+//                 balance -= amount
+//                 console.log(`You are withdwar ${amount} rupee and you updated balance is : ${balance}`)
+//             },
+
+//             currentBalance(){
+//                 console.log(`Your current balance is : ${balance}`)
+//             }
+//         }
+//     }
+
+// function doTransaction(){
+
+//     const account = backAccount() // closure created
+
+//     account.deposite(2000)
+//     account.withdraw(200)
+//     account.currentBalance()
 // }
 
-// execute(function(){
-//     console.log("Executed")
-// });
+// doTransaction()
 
-//* Q2. Create a function calculate that takes three parameters:
-//* two numbers
-//* one callback function
-//* The callback decides the operation.
+//? Nested closure question
 
-// function calculate(a, b, operation){
-//     operation(Number(a), Number(b))
+// function outer() {
+//     let count = 0;
+
+//     return function inner() {
+//         count++;
+//         console.log(count);
+
+//         return function deeper() {
+//             count++;
+//             console.log(count);
+//         };
+//     };
 // }
 
-// calculate(4, 5, function(a, b){
-//     console.log(a + b)
-// })
+// const fn = outer(); // outer function call & return inner function & assign to the fn * also remember count value = 0
 
-//* Q3. Write a function greetUser that takes name and callback.
-//* Callback should modify the name before printing.
+// const a = fn(); // inner funtion call & deepar funtion assign to the a * also remember count value = 1
+// const b = fn(); // same -> inner funtion call & deepar funtion assign to the b * also remember count value = 2
 
-// function greetUser(name, callback){
-//     name = "Saloni Saini"
-//     callback(name);
+// a(); // last deeper function call & return undefined * also remember last count value = 2 and then count value is = 3
+// b(); // last deeper function call & return undefined * also remember last count value = 3 and then count value is = 4
+
+//? Tumhe ek aisa system banana hai jisme ek variable (jaise password ya balance) direct access nahi hona chahiye.
+//? 👉 Requirements:
+//? direct access ❌
+//? sirf controlled functions ke through access ✔️
+//? 👉 Socho:
+//? closure ka use kahan hoga?
+
+// function systemCheck(){
+//     const pass = 1234
+
+//     return function check(check){
+
+//         if(isNaN(check)) return console.error("Plase enter first your passward");
+
+//         if(pass===check)return console.log("Access Granted");
+//         return console.log("Access Denaid!")
+//     }
 // }
 
-// greetUser("Rajat", function(modifyName){
-//     console.log(modifyName)
-// })
+// const system = systemCheck();
 
-//* Q4. Use forEach with an arrow callback to print squares of numbers in [1,2,3,4].
+// // Enter passward First
+// system("1234");
 
-// let arr = [1,2,3,4]
-// arr.forEach((val)=>{
-//     console.log(val ** 2)
-// })
+//? Ek web page me button hai. Har click pe count badhna chahiye.
+//? 👉 Requirements:
+//? global variable use nahi karna
+//? multiple buttons ho sakte hain
+//? 👉 Socho:
+//? har button ka count alag kaise hoga?
 
-//* Q5. Write a function runTwice that takes a callback and executes it two times.
+// function countCheck(){
 
-// function runTwice(callback){
-//     callback();
-//     callback();
+//     let count = 0
+
+//     return function counter(){
+//         count++
+//         console.log(count)
+//     }
+
 // }
 
-// runTwice(function(){
-//     console.log("Execute")
-// })
-
-//* Q6. Create a function greet that takes a name and a callback.
-//* The callback should transform the name before printing it.
-//* Example idea:
-//* original: "rajat"
-//* callback makes it uppercase
-
-// function greet(name, callback){
-//     callback(name.toUpperCase());
+// function button(){
+//     const btn = countCheck()
+//     btn()
+//     btn()
 // }
 
-// greet("Rajat Saini", function(uppercase){
-//     console.log(uppercase)
-// })
+// button()
+// button()
 
-//* Q7. Write a function applyOperation that takes:
-//* two numbers
-//* one callback
-//* Return the result of the callback.
+//? Tum ek function bana rahe ho jo configuration accept karta hai (jaise tax rate, discount, etc.) aur ek naya function return karta hai.
+//? 👉 Question:
+//? returned function configuration kaise yaad rakhega?
 
-// function applyOperation(a, b, callback){
-//     callback(Number(a), Number(b));
+// function configuraiton(a){
+//     return function(b){
+//         return function(c){
+
+//             console.log(a,b,c)
+//         }
+//     }
 // }
 
-// applyOperation(4,5, function(x, y){
-//     console.log( x ** y)
-// })
+// let check = configuraiton(1)(3)
+//  check(4)
 
-//* Q9. Use rest parameter to accept any number of scores and return the total.
+//? Ek function banao jo message le
+//? callback ke through delay ke baad print kare
 
-// function getScore(...scores){
-//     let total = 0;
-//     scores.forEach(function(val){
-//         total = total + val
-//     })
+// function mesg(msg){
+//     setTimeout(() => {
+//         console.log(msg)
+//     },1000);
 
-//     return total
 // }
 
-// let sum = getScore(1,2,3,4,5,6)
+// mesg("Hello")
 
-// console.log(sum)
+//? Ek function banao jo ek number le aur ek function return kare
+//? returned function dusra number le
+//? dono numbers multiply kare
 
-//* Q10. Fix the funtion with early return
-//* function checkAge(age){
-//*     if(age < 18){
-//*         console.log("Too young")
-//*     }else{
-//*         console.log("Allowed")
-//*     }
-//* }
+// function check(num1){
+//     return function(num2){
+//         let mul = num1 * num2
+//         return mul
+//     }
+// }
 
-// const checkAge = (age) => age < 18 ? "Too young" : "Allowed";
-// console.log(checkAge(20))
+// let checkResult = check(2);
 
-//* Q11. Create a function processArray that takes:
-//* an array
-//* a callback
-//* Apply the callback to each element using a loop (not map).
+// console.log(checkResult(3));
 
-// let num = [1, 2, 3, 4, 5];
+//? Ek function banao jo:
+//? ek callback le
+//? ek delay le
+//? delay ke baad callback run kare
 
-// function processArray(arr, callback) {
-//   let result = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     result.push(callback(arr[i]));
+// function check(callback, delay) {
+//   setTimeout(callback, delay);
+// }
+
+// check(function () {
+//     console.log("Hello");
+//   },2000);
+
+//? Counter banao jo:
+//? ek limit le
+//? limit ke baad increment na kare
+
+
+// function counter(){
+//   let count = 0;
+
+//   return function(){
+//     if(count >= 6){
+//       return "Limit reached";
+//     }
+//     return ++count;
 //   }
-//   return result;
 // }
 
-// let squared = processArray(num, function (num) {
-//   return num * num;
-// });
+// let check = counter();
 
-// console.log(squared);
+// console.log(check()); 
+// console.log(check()); 
+// console.log(check()); 
+// console.log(check()); 
+// console.log(check()); 
+// console.log(check()); 
+// console.log(check()); 
 
 
+//? Ek function banao jo:
+//? ek secret value store kare
+//? sirf ek function ke through read/update ho
 
-//* Q13. Create your own version of map:
-//* Function name: CustomMap
-//* It should take array + callback and return a new transformed array.
+// function checkCurrentStatus(){
+//   let secretVal = 1234
 
-// let arr = [1, 2, 3, 4];
-// function customMap(arr, callback) {
-//   callback();
+//   return{
+//     read(){
+//       return secretVal;
+//     },
+//     update(upd){
+//       return secretVal = upd
+//     }
+//   }
 // }
 
-// customMap(arr, function (newArr) {
-//   console.log(newArr);
-// });
+// let check = checkCurrentStatus()
 
+// console.log(check.read())
+// console.log(check.update(2345))
+
+
+//? Ek function banao jo:
+//? ek callback le
+//? ek number n le
+//? callback ko n times run kare
+
+
+// function takes(callback, n){
+
+//     for(let i = 1; i<=n; i++){
+//       callback(i);
+//     }
+
+// }
+
+// takes(function(e){
+//   console.log(`Callback run : ${e} times`)
+// },6)
+
+
+
+//? Ek function banao jo:
+//? har input store kare
+//? ek function return kare jo history dikha sake
+
+// function historyTracker(...inp){
+//   let input = inp
+
+//   return input
+
+// }
+
+// console.log(`Input History => ${historyTracker("rajat", 1,2,3,true)}`)
+
+//? Username Generator
+//? Ek function:
+//? input: full name "Rohit Sharma"
+//? output: "rohit_123" (first name lowercase + random number)
+
+// function userName(fullName){
+//     const first = fullName.trim().toLowerCase().split(" ")[0];
+//     const random = Math.floor(Math.random() * 1000);
+
+//     console.log(`Your FullName is : ${fullName} ...and\nYour userName is now : ${first}_${random}`);
+// }
+
+// userName("Rajat Saini");
+// userName("Anil");
+
+
+
+// function useState(initialValue) {
+
+//   let state = initialValue;
+
+//   return [
+//     function getState() {
+//       return state;
+//     },
+//     function updateValue(newValue) {
+//       state = newValue;
+//     }
+//   ];
+// }
+
+// const [getState, updateValue] = useState(0);
+
+
+// updateValue(10);
+
+// console.log(getState());
